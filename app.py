a2c4af2a56a5cddec52ddb55454241c6f9446d91
curiousAhmed <91704314+curiousAhmed@users.noreply.github.com>
2023-07-26 15:38:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, jsonify, request
-#from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-#cors = CORS(app)  # This enables CORS for all routes.
 
-# Your route and other code here...
+@app.route('/')
+def index():
+    return render_template('ucpage.html')
+
 @app.route('/store', methods=['POST'])
-def store_data():
-    data = request.get_json()
-    email = data['email']
-    # Here, you can store the email in a file or database.
-    # For simplicity, we'll just print the email for now.
-    print('Received email:', email)
-    return jsonify(message='Email stored successfully.')
+def store_email():
+    if request.method == 'POST':
+        data = request.get_json()
+        email = data.get('email')
+        if email:
+            # Do something with the email data (e.g., store it in a database)
+            # For demonstration, we'll just return the email back to the client
+            return jsonify({'message': f'Received email: {email}'}), 200
+        else:
+            return jsonify({'message': 'Email not provided'}), 400
+    else:
+        return jsonify({'message': 'Method not allowed'}), 405
+
 if __name__ == '__main__':
     app.run(debug=True)
